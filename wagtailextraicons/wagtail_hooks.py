29543007d8adf0fba1f1d0350c3a99b5655ea1c9
Sam Costigan <sam@kernl.co.nz>
2020-01-27 17:05:49 +1300

    Add support for earlier wagtail and django versions, bump package version

--- wagtailextraicons/wagtail_hooks.py
+++ wagtailextraicons/wagtail_hooks.py
@@ -1,12 +1,31 @@
 from django.utils.html import format_html
-from django.contrib.staticfiles.templatetags.staticfiles import static
+from pkg_resources import parse_version
 
-from wagtail.core import hooks
+try:
+    from django.templatetags.static import static
+except ImportError:  # Fallback for Django <2.1
+    from django.contrib.staticfiles.templatetags.staticfiles import static
+
+try:
+    from wagtail import __version__ as WAGTAIL_VERSION
+    from wagtail.core import hooks
+except ImportError:  # Fallback for Wagtal <2.0
+    from wagtail.wagtailcore import __version__ as WAGTAIL_VERSION
+    from wagtail.wagtailcore import hooks
 
 
-@hooks.register('insert_global_admin_css')
-def extra_icons_css():
+def extraicons_stylesheet():
     return format_html(
         '<link rel="stylesheet" href="{}">',
         static('wagtailextraicons/css/main.css')
     )
+
+
+# New Wagtail versions support importing CSS throughout the admin.
+# Fall back to the old hook (editor screen only) for older versions.
+if parse_version(WAGTAIL_VERSION) >= parse_version('1.4'):
+    admin_stylesheet_hook = 'insert_global_admin_css'
+else:
+    admin_stylesheet_hook = 'insert_editor_css'
+
+hooks.register(admin_stylesheet_hook, extraicons_stylesheet)
