fa5947797c8ead39d6d00e1c3133ba6dc671a483
Pat Horsley <pat@octave.nz>
2022-02-21 14:35:49 +1300

    Implement SVG icon build task.

--- wagtailextraicons/__init__.py
+++ wagtailextraicons/__init__.py
@@ -1,2 +1,2 @@
-__version__ = '0.1.7'
+__version__ = '2.0.0-alpha'
 default_app_config = 'wagtailextraicons.apps.WagtailExtraIconsConfig'
--- wagtailextraicons/wagtail_hooks.py
+++ wagtailextraicons/wagtail_hooks.py
@@ -1,31 +1,16 @@
-from django.utils.html import format_html
 from pkg_resources import parse_version
 
-try:
-    from django.templatetags.static import static
-except ImportError:  # Fallback for Django <2.1
-    from django.contrib.staticfiles.templatetags.staticfiles import static
+from wagtail import __version__ as WAGTAIL_VERSION
+from wagtail.core import hooks
 
-try:
-    from wagtail import __version__ as WAGTAIL_VERSION
-    from wagtail.core import hooks
-except ImportError:  # Fallback for Wagtal <2.0
-    from wagtail.wagtailcore import __version__ as WAGTAIL_VERSION
-    from wagtail.wagtailcore import hooks
+if parse_version(WAGTAIL_VERSION) <= parse_version('2.15'):
+    raise Exception('wagtailextraicons 2 requires Wagtail > 2.15')
 
+# Note: .icon_register.py is generated in the build task of this package
+from .icon_register import icons
 
-def extraicons_stylesheet():
-    return format_html(
-        '<link rel="stylesheet" href="{}">',
-        static('wagtailextraicons/css/main.css')
-    )
-
-
-# New Wagtail versions support importing CSS throughout the admin.
-# Fall back to the old hook (editor screen only) for older versions.
-if parse_version(WAGTAIL_VERSION) >= parse_version('1.4'):
-    admin_stylesheet_hook = 'insert_global_admin_css'
-else:
-    admin_stylesheet_hook = 'insert_editor_css'
-
-hooks.register(admin_stylesheet_hook, extraicons_stylesheet)
+@hooks.register('register_icons')
+def register_icons(_icons):
+    for icon in icons:
+        _icons.append("extraicons/{}".format(icon))
+    return _icons
